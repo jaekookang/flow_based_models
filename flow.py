0b860ab86f753bd59399a21ed5984ffcec2bd30f
jaekookang <jkang@gradcenter.cuny.edu>
2020-11-22 15:44:40 +0900

    update from walnut

---
+++ flow.py
@@ -0,0 +1,131 @@
+'''
+Flow based models
+
+- NICE
+- RealNVP
+
+2020-11-18 first created
+'''
+
+import tensorflow as tf
+from utils import *
+
+tfk = tf.keras
+tfkl = tfk.layers
+tfkc = tfk.callbacks
+K = tfk.backend
+
+
+def fully_connected(n_dim, n_layer=3, n_hid=512, activaiton='relu'):
+    '''Fully connected neural networks'''
+    nn = tfk.Sequential(name='neural_net')
+    for _ in range(n_layer - 1):
+        nn.add(tfkl.Dense(n_hid, activation=activaiton))
+    nn.add(tfkl.Dense(n_dim//2, activation='linear'))
+    return nn
+
+
+class AdditiveCouplingLayer(tfkl.Layer):
+    '''
+    Implementation of Additive Coupling layers in Dinh et al (2015)
+
+    # forward
+    y1 = x1
+    y2 = x2 + m(x1)
+    # inverse
+    x1 = y1
+    x2 = y2 - m(y1)
+    '''
+
+    def __init__(self, inp_dim, n_hid_layer, n_hid_dim, name):
+        super(AdditiveCouplingLayer, self).__init__(name=name)
+        self.inp_dim = inp_dim
+        self.n_hid_layer = n_hid_layer
+        self.n_hid_dim = n_hid_dim
+        self.m = fully_connected(inp_dim, n_hid_layer, n_hid_dim)
+        self.permute = tfkl.Lambda(lambda x: tf.gather(
+            x, list(reversed(range(x.shape[-1]))), axis=-1))
+
+    def call(self, x):
+        x = self.permute(x)
+        x1, x2 = self.split(x)
+        mx1 = self.m(x1)
+        y1 = x1
+        y2 = x2 + mx1
+        y = tf.concat([y1, y2], axis=-1)
+        return y
+
+    def inverse(self, y):
+        y1, y2 = self.split(y)
+        my1 = self.m(y1)
+        x1 = y1
+        x2 = y2 - my1
+        x = tf.concat([x1, x2], axis=-1)
+        x = self.permute(x)
+        return x
+
+    def split(self, x):
+        dim = self.inp_dim
+        x = tf.reshape(x, [-1, dim//2, 2])
+        return x[:,:,0], x[:,:,1]
+
+
+class ScalingLayer(tfkl.Layer):
+    def __init__(self, inp_dim):
+        super(ScalingLayer, self).__init__(name='ScalingLayer')
+        self.inp_dim = inp_dim
+        self.scaling = tf.Variable(shape=(inp_dim,),
+                                   trainable=True,
+                                   initial_value=tfk.initializers.glorot_uniform()((inp_dim,)))
+
+    def call(self, x):
+        self.add_loss(-tf.math.reduce_sum(self.scaling))
+        return tf.math.exp(self.scaling) * x
+
+    def inverse(self, y):
+        return tf.math.exp(-self.scaling) * y
+
+
+class RealNVP(tfkl.Layer):
+    pass
+
+
+class NICE(tfk.Model):
+    def __init__(self, inp_dim, n_couple_layer, n_hid_layer, n_hid_dim, **kwargs):
+        super(NICE, self).__init__(**kwargs)
+        self.inp_dim = inp_dim
+        self.n_couple_layer = n_couple_layer
+        self.n_hid_layer = n_hid_layer
+        self.n_hid_dim = n_hid_dim
+        self.AffineLayers = []
+        for i in range(n_couple_layer):
+            layer = AdditiveCouplingLayer(inp_dim, n_hid_layer, n_hid_dim, name=f'Layer{i}')
+            self.AffineLayers.append(layer)
+        self.scale = ScalingLayer(inp_dim)
+        self.AffineLayers.append(self.scale)
+
+    def call(self, x):
+        '''Forward: data (x) --> latent (z); inference'''
+        z = x
+        for i in range(self.n_couple_layer):
+            z = self.AffineLayers[i](z)
+        z = self.scale(z)
+        return z
+
+    def inverse(self, z):
+        '''Inverse: latent (z) --> data (y); sampling'''
+        x = self.scale.inverse(z)
+        for i in reversed(range(self.n_couple_layer)):
+            x = self.AffineLayers[i].inverse(x)
+        return x
+
+
+if __name__ == "__main__":
+    inp_dim = 2
+    n_couple_layer = 3
+    n_hid_layer = 3
+    n_hid_dim = 512
+    model = NICE(inp_dim, n_couple_layer, n_hid_layer, n_hid_dim)
+    x = tfkl.Input(shape=(inp_dim,))
+    model(x)
+    model.summary()
--- utils.py
+++ utils.py
@@ -5,9 +5,11 @@ Utilities
 '''
 
 import matplotlib.pyplot as plt
+import matplotlib
 import seaborn as sns
 import os
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 from time import time, strftime, gmtime
 import tensorflow as tf
 tfk = tf.keras
@@ -16,7 +18,7 @@ tfkc = tfk.callbacks
 
 class NBatchLogger(tfkc.Callback):
     '''A Logger that log average performance per `display` steps.
-    
+
     See: https://gist.github.com/jaekookang/7e2ca4dc2b1ab10dbb80b9e65ca91179
     '''
 
@@ -80,104 +82,106 @@ class NBatchLogger(tfkc.Callback):
             self.fid.flush()
 
 
-def visualize_from_latent(model, n_data, title_txt=None, sharexy=False, seed=None):
-    # This function applies to NICE model
-    # "inverse" model: latent --> data
-    #
-    # # - model: NICE, RealNVP
-    # - n_data: number of samples to use
-    
+def visualize_moon(model, n_sample, direction='forward', sharexy=True, seed=0, xlim=[-4, 4], ylim=[-4, 4]):
+    '''Visualize model prediction on moon-shaped dataset
+    model: keras model
+    n_sample: samples to generate
+    directoin: 'forward' means data (x) to latent (z); default
+               'inverse' means latent (z) to data (x)
+    '''
+    color_list = ['red', 'green', 'blue', 'black']
+
     if seed is not None:
         # if you want to control the randomness
         np.random.seed(seed)
-    init_sample = np.random.randn(n_data, 2).astype('float32')
 
     samples = []
     names = []
-    samples.append(init_sample)
-    names.append('latent')
-    x = init_sample
+    if direction == 'forward':
+        x2 = np.random.normal(0, 4, n_sample)
+        x1 = np.random.normal(0.25 * x2**2, [1]*n_sample)
+        moon = np.stack([x1, x2], axis=1).astype('float32')
+        moon = StandardScaler().fit_transform(moon)
+        samples.append(moon)
+        names.append('data')
+        x = moon
+        for layer in model.layers:
+            x = layer(x).numpy()
+            samples.append(x)
+            names.append(layer.name)
+
+    elif direction == 'inverse':
+        init_sample = np.random.randn(n_sample, 2).astype('float32')
+        samples.append(init_sample)
+        names.append('latent')
+        x = init_sample
+        for layer in reversed(model.layers):
+            x = layer.inverse(x).numpy()
+            samples.append(x)
+            names.append(layer.name)
+
+    fig, arr = plt.subplots(1, len(model.layers)+1, facecolor='white',
+                            figsize=(3.5 * (len(model.layers)+1), 3.5),
+                            sharex=sharexy, sharey=sharexy)
+
+    # Divide 4 quadrants for tracing transformation
+    X0 = samples[0]
+    for i in range(len(samples)):
+        X1 = samples[i]
+        idx = np.logical_and(X1[:, 0] < 0, X0[:, 1] < 0)
+        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color=color_list[0])
+        idx = np.logical_and(X0[:, 0] > 0, X0[:, 1] < 0)
+        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color=color_list[1])
+        idx = np.logical_and(X0[:, 0] < 0, X0[:, 1] > 0)
+        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color=color_list[2])
+        idx = np.logical_and(X0[:, 0] > 0, X0[:, 1] > 0)
+        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color=color_list[3])
+        arr[i].set_title(names[i])
+        if sharexy:
+            arr[i].set_xlim(xlim)
+            arr[i].set_ylim(ylim)
+    return fig
+
+def animate_moon_gif(model, n_sample, n_couple_layer, filename=None):
+    # Make GIF file
+    # - Do `brew install imagemagick` or `apt install imagemagick` first
+    from matplotlib import animation
+    from IPython.display import HTML
+    interval = 100
+    xlim = [-4, 4]
+    ylim = [-4, 4]
+
+    fig, ax = plt.subplots()
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+    z = np.random.randn(n_sample, 2).astype('float32')
+    results = [z]
     for layer in reversed(model.layers):
-        x = layer.inverse(x).numpy()
-        samples.append(x)
-        names.append(layer.name)
+        z = layer.inverse(z).numpy()
+        results += [z]
 
-    if sharexy:
-        fig, arr = plt.subplots(1, len(model.layers)+1, facecolor='white',
-                                figsize=(3.5 * (len(model.layers)+1), 3.5),
-                                sharex=True, sharey=True)
-    else:
-        fig, arr = plt.subplots(1, len(model.layers)+1, facecolor='white',
-                                figsize=(3.5 * (len(model.layers)+1), 3.5))
-    if title_txt is not None:
-        plt.suptitle(title_txt, fontsize=20, y=1.05)
+    y = results[0]
+    paths = ax.scatter(y[:, 0], y[:, 1], s=5, color='coral')
 
-    # Divide 4 quadrants for tracing transformation
-    X0 = samples[0]
-    for i in range(len(samples)):
-        X1 = samples[i]
-        idx = np.logical_and(X1[:, 0] < 0, X0[:, 1] < 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='red')
-        idx = np.logical_and(X0[:, 0] > 0, X0[:, 1] < 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='green')
-        idx = np.logical_and(X0[:, 0] < 0, X0[:, 1] > 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='blue')
-        idx = np.logical_and(X0[:, 0] > 0, X0[:, 1] > 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='black')
-        arr[i].set_title(names[i])
-    return fig
+    def animate(i):
+        n = 40
+        l = i//n
+        t = (float(i % n))/n
+        y = (1-t)*results[l] + t*results[l+1]
+        paths.set_offsets(y)
+        return (paths,)
+
+    anim = animation.FuncAnimation(
+        fig, animate, frames=40*(n_couple_layer+1), interval=interval, blit=False);
+    if filename is not None:
+        anim.save(filename, writer='imagemagick', fps=60)
+        print(filename, 'saved')
+    #HTML(anim.to_html5_video())
+    return anim
 
 
-def visualize_from_data(model, n_data, title_txt=None, sharexy=False, seed=None):
-    # This function applies to NICE model
-    # "forward" model: data --> latent
-    # 
-    # - model: NICE, RealNVP
-    # - n_data: number of samples to use
-    
-    if seed is not None:
-        # if you want to control the randomness
-        np.random.seed(seed)
-    latent = np.random.randn(n_data, 2).astype('float32')
-    init_sample = model.inverse(latent).numpy()
-
-    samples = []
-    names = []
-    samples.append(init_sample)
-    names.append('data')
-    x = init_sample
-    for layer in model.layers:
-        x = layer(x).numpy()
-        samples.append(x)
-        names.append(layer.name)
-
-    if sharexy:
-        fig, arr = plt.subplots(1, len(model.layers)+1, facecolor='white',
-                                figsize=(3.5 * (len(model.layers)+1), 3.5),
-                                sharex=True, sharey=True)
-    else:
-        fig, arr = plt.subplots(1, len(model.layers)+1, facecolor='white',
-                                figsize=(3.5 * (len(model.layers)+1), 3.5))
-    if title_txt is not None:
-        plt.suptitle(title_txt, fontsize=20, y=1.05)
-
-    # Divide 4 quadrants for tracing transformation
-    X0 = samples[0]
-    for i in range(len(samples)):
-        X1 = samples[i]
-        idx = np.logical_and(X1[:, 0] < 0, X0[:, 1] < 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='red')
-        idx = np.logical_and(X0[:, 0] > 0, X0[:, 1] < 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='green')
-        idx = np.logical_and(X0[:, 0] < 0, X0[:, 1] > 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='blue')
-        idx = np.logical_and(X0[:, 0] > 0, X0[:, 1] > 0)
-        arr[i].scatter(X1[idx, 0], X1[idx, 1], s=10, color='black')
-        arr[i].set_title(names[i])
-    return fig
-
-
-def visualize_forward_gauss(gauss_data, labels, model, sharexy=False, colormap='Set1'):
+def visualize_forward_gauss(gauss_data, labels, model, sharexy=True, colormap='Set1'):
     # gauss_data: input data (X)
     # labels: label for each example in init_data (np.array of integer)
     x = gauss_data.reshape((-1, 2))
@@ -191,31 +195,31 @@ def visualize_forward_gauss(gauss_data, labels, model, sharexy=False, colormap='
         x = layer(x)
         samples.append(x.numpy())
         names.append(layer.name)
-        
+
     if sharexy:
         fig, arr = plt.subplots(1, len(samples), facecolor='white',
-                              figsize=(3.5 * (len(samples)+1), 3.5),
-                              sharex=True, sharey=True)
+                                figsize=(3.5 * (len(samples)+1), 3.5),
+                                sharex=True, sharey=True)
     else:
         fig, arr = plt.subplots(1, len(samples), facecolor='white',
-                              figsize=(3.5 * (len(samples)+1), 3.5))
+                                figsize=(3.5 * (len(samples)+1), 3.5))
 
     cmap = sns.color_palette(colormap, len(uq_labels))
     plt.suptitle('Data --> Latent', fontsize=20, y=1.05)
-    
+
     # Iterate over layer activations
     for i, ax in zip(range(len(samples)), arr):
         # Iterate over classes
         ax.set_title(names[i])
         for label, c in zip(uq_labels, cmap):
-            sample = samples[i][labels==label,:]
-            ax.scatter(sample[:,0], sample[:,1], s=10, color=c)
+            sample = samples[i][labels == label, :]
+            ax.scatter(sample[:, 0], sample[:, 1], s=10, color=c)
     return fig
 
 
-def visualize_inverse_gauss(n_sample, model, sharexy=False, color_list=['red','green','blue','black'], seed=None):
+def visualize_inverse_gauss(n_sample, model, sharexy=False, color_list=['red', 'green', 'blue', 'black'], seed=None):
     # n_sample: number of random samples
-    
+
     if seed is not None:
         # if you want to control the randomness
         np.random.seed(seed)
@@ -266,7 +270,7 @@ def visualize_mnist_layers(model, dataset, direction='inverse', sharexy=True, cm
     '''
     samples = []
     names = []
-    if direction == 'forward':        
+    if direction == 'forward':
         it = iter(dataset)
         x = next(it)[0].numpy()[0]
         samples += [x]
@@ -275,7 +279,7 @@ def visualize_mnist_layers(model, dataset, direction='inverse', sharexy=True, cm
             x = layer(x)
             samples.append(x.numpy())
             names.append(layer.name)
-            
+
     elif direction == 'inverse':
         n_samples = 784
         x = np.random.randn(n_samples) * 0.75
@@ -289,11 +293,11 @@ def visualize_mnist_layers(model, dataset, direction='inverse', sharexy=True, cm
 
     if sharexy:
         fig, arr = plt.subplots(1, len(samples), facecolor='white',
-                              figsize=(4 * (len(samples)), 4),
-                              sharex=True, sharey=True)
+                                figsize=(4 * (len(samples)), 4),
+                                sharex=True, sharey=True)
     else:
         fig, arr = plt.subplots(1, len(samples),  facecolor='white',
-                              figsize=(4 * (len(samples)), 4))
+                                figsize=(4 * (len(samples)), 4))
 
     cmap = matplotlib.cm.get_cmap(cmap)
     cmap = cmap.reversed()
